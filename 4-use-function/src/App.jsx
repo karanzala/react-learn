6d3347636a557512a1eeec22d6fe6bada260a5bf
karanzala <zalakaransinh777@gmail.com>
2026-03-30 16:08:01 +0530

    complited use of function

--- 4-use-function/src/App.jsx
+++ 4-use-function/src/App.jsx
@@ -1,35 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState();
+  const [active, setActive] = useState(false);
 
+  // function handleClick(){
+  //   console.log("Button double clicked!");
+  // }
+  const getValue = () =>{
+    setActive(true)
+  }
+
+  const buttonClear = () =>{
+    setData("");
+    setActive(false);
+    document.getElementById("name").value = "";
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>Hello, React! use function</h1>
+        <input
+          type="text"
+          name=""
+          onChange={(e) => {
+            setData(e.target.value);
+            setActive(false);
+          }}
+          id="name"
+        />
+        {/* <button onClick={getValue} onDoubleClick={handleClick}> */}
+        <button onClick={getValue}>
+          Click me
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={buttonClear}>Clear</button>
+        {active && <p>{data || "No data available"}</p>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
